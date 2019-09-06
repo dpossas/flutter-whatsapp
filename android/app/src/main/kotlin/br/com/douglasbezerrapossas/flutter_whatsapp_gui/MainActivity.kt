8dc127a7ffa2e9d149b01b6e898eeab6501683f3
= <douglas.possas@projuris.com.br>
2019-09-06 13:35:45 -0300

    Projeto inicial

---
+++ android/app/src/main/kotlin/br/com/douglasbezerrapossas/flutter_whatsapp_gui/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.douglasbezerrapossas.flutter_whatsapp_gui
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
